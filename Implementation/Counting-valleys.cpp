077365bf5820f0dba3de5b827c245fbe5c0fa1a6
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-09-16 20:20:29 +0530

    Create Counting-valleys.cpp

---
+++ Implementation/Counting-valleys.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int l;
+    string str; cin>>l>>str;
+    int height = 0;
+    int count = 0;
+    for(int i=0;i<l;i++){
+        if (str[i]=='U') height++;
+        else {
+            if (height==0) count++;
+            height--;
+        }
+    }
+    if (height<0) count--;
+    cout<<count<<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
