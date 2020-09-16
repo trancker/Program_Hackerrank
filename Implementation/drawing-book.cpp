f7401766e374a813472d795909b325f303069bf2
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-09-16 20:18:24 +0530

    Create drawing-book.cpp

---
+++ Implementation/drawing-book.cpp
@@ -0,0 +1,35 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int p;
+    cin >> p;
+    cout<<min(p/2,(n-p)/2);
+    // your code goes here
+    return 0;
+}
