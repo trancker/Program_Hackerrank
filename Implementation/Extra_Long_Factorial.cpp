cd40b082bcf9f95063f86aa135cb533833f851a6
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-09-18 12:40:31 +0530

    Create Extra_Long_Factorial.cpp

---
+++ Implementation/Extra_Long_Factorial.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the extraLongFactorials function below.
+void extraLongFactorials(int n) {
+
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    extraLongFactorials(n);
+
+    return 0;
+}
