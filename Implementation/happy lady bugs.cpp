b22ced40c721eb07b6ed95c302054703cdc4b708
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-11-14 19:10:36 +0530

    Create happy lady bugs.cpp
    this project is added after 2 months of time

---
+++ Implementation/happy lady bugs.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the happyLadybugs function below.
+string happyLadybugs(string b) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int g;
+    cin >> g;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int g_itr = 0; g_itr < g; g_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string b;
+        getline(cin, b);
+
+        string result = happyLadybugs(b);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
