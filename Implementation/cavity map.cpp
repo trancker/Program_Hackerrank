60bee0f68c96c1ba14685b5121fe8696de40b187
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-11-14 19:06:32 +0530

    Create cavity map.cpp
    This is updated after 2 months

---
+++ Implementation/cavity map.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the cavityMap function below.
+vector<string> cavityMap(vector<string> grid) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> grid(n);
+
+    for (int i = 0; i < n; i++) {
+        string grid_item;
+        getline(cin, grid_item);
+
+        grid[i] = grid_item;
+    }
+
+    vector<string> result = cavityMap(grid);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
