10c24ed244f9dd9ae245dfd4d9d5eb10fd4f4440
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-11-14 19:14:23 +0530

    Create starnge counter.cpp
    This project  is created after 2 months od time

---
+++ Implementation/starnge counter.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the strangeCounter function below.
+long strangeCounter(long t) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    long t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
